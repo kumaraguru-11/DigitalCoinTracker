9aaf95466be6ee325f158cbcb64bb4ce705f71cd
kumaraguru-11 <kumaraguru1181@gmail.com>
2024-04-16 13:55:47 -0200

    Fix news card error

--- src/Component/NewsCard.js
+++ src/Component/NewsCard.js
@@ -3,31 +3,37 @@ import Loader from "./feature/Loader";
 import { useLocation } from "react-router-dom";
 import TimeAgo from "timeago-react";
 
+// `https://cryptocurrency-news2.p.rapidapi.com/v1/coindesk`
+//x-rapidapi Host:cryptocurrency-news2.p.rapidapi.com
 const NewsCard = () => {
   const [news, setNews] = React.useState("");
   const location = useLocation();
-  const news2 = location.pathname !== "/news" ? news.slice(0, 6) : news;
+  const news2 = location.pathname !== "/news" && news.status==='success'? news.slice(0, 6) : news;
   React.useEffect(() => {
-    fetch(`https://cryptocurrency-news2.p.rapidapi.com/v1/coindesk`, {
-      headers: {
-        "X-RapidAPI-Key": "ffc6c94612msh07710f41c73452dp1126fdjsn1b944e487f04",
-        "X-RapidAPI-Host": "cryptocurrency-news2.p.rapidapi.com",
-      },
-    })
+    fetch(
+      "https://google-news13.p.rapidapi.com/search?keyword=cryptocurrency&lr=en-US",
+      {
+        headers: {
+          "X-RapidAPI-Key":
+            "ffc6c94612msh07710f41c73452dp1126fdjsn1b944e487f04",
+          "X-RapidAPI-Host": "google-news13.p.rapidapi.com",
+        },
+      }
+    )
       .then((resp) => resp.json())
       .then((res) => {
-        setNews(res.data);
+        setNews(res);
       });
   }, []);
 
   return (
     <div className="news-continer">
       <div className="news-grid">
-        {news2 ? (
-          news2.map((el) => (
-            <div className="news-card" key={el.url}>
+        {news2 && news.status === "success" ? (
+          news2.items.map((el) => (
+            <div className="news-card" key={el.newsUrl}>
               <a
-                href={el.url}
+                href={el.newsUrl}
                 target="_blank"
                 rel="noreferrer"
                 title={el.title}
@@ -37,36 +43,30 @@ const NewsCard = () => {
                     <div className="news-header">
                       <h2>{el.title}</h2>
                       <img
-                        src={el.thumbnail && el.thumbnail}
-                        alt="imgage not found"
+                        src={el.images?.thumbnailProxied}
+                        alt="img404"
                         width="100"
                         height="100"
                       />
                     </div>
                     <div className="news-published-detail">
-                      {/* <div className="news-published-person-detail">
-                        <span className="news-card-icon">
-                          <i className="bi bi-person"></i>
-                        </span>
-                        <span>{el.title}</span>
-                      </div> */}
                       <div className="news-published-time-detail">
                         <span className="news-card-icon">
                           <i className="bi bi-calendar4"></i>
                         </span>
-                        {/* <span>{el.createdAt}</span> */}
                         <span>
-                          <TimeAgo datetime={el.createdAt} />
+                          <TimeAgo datetime={el.timestamp} />
                         </span>
-                        {/* <span>{timeAgo(el.createdAt)}</span> */}
                       </div>
                     </div>
-                    <p>{el.description}</p>
+                    <p>{el.snippet}</p>
                   </div>
                 </div>
               </a>
             </div>
           ))
+        ) : news.message || news.status !== "success" ? (
+          <h1>unavailable</h1>
         ) : (
           <Loader />
         )}
